ab9affba693cffa5fee7ae53257f84a7e74c2995
RamokTVL <sex@ramok.fr>
2022-04-10 01:33:22 +0200

    🔊 Added logs for refreshConfiguration function

--- src/structures/functions/refreshConfiguration.ts
+++ src/structures/functions/refreshConfiguration.ts
@@ -1,7 +1,10 @@
 import { VintedApi } from "../VintedApi";
 
 export function refreshConfiguration(vintedClient:VintedApi) {
+    vintedClient.logger.log("Refreshing configuration", "INFO");
     delete require.cache[require.resolve("../../config.json")];
+    vintedClient.logger.log("Deleted require cache for config.json", "INFO");
     const config:Configuration = require("../../config.json");
     vintedClient.configuration = config;
+    vintedClient.logger.log("Updated configuration", "OK");
 }
